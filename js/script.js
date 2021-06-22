ac341288a1d6150ef0146ed78e9ddd6f3a3d452b
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-06-22 06:17:34 +0300

    Remove commented out code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+
+
+// USER INTERFACE LOGIC
+function showMessage(){
+    alert("Hello!,Your Message has been received");
+}
+
 $('.switch').hover(function() {
     $(this).closest('.showWords').hide();
     $(this).next('div').closest('.portfolioParagraph').show();
@@ -7,10 +14,5 @@ $('.switch').hover(function() {
     $(this).closest('.showWords').show();
 });
 
-function showMessage(){
-    alert("Hello!,Your Message has been received");
-}
-
-// (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
 
 
